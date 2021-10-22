e79e63fdc2d16ca10442096998186926743a6c7a
Chris <github@c4artz.de>
2021-10-22 13:55:24 +0200

    added favicon and simple nav

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const copyright = `Copyright &copy ${new Date().getFullYear()} C4ArtZ - All Rights reserved.`
+
+const textElement = document.querySelector('#copyright');
+
+textElement.innerHTML = copyright;
